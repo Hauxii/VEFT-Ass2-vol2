4d96a0576a732e4a0cee9aee2797f936b4d798b4
Hauxii <haukur91@gmail.com>
2016-09-02 11:23:02 +0000

    git init

---
+++ API/Controllers/CoursesController.cs
@@ -0,0 +1,6 @@
+
+
+namespace Ass2.API.Controllers
+{
+    
+}
---
+++ Models/CourseLiteDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ass2.Models
+{
+    public class CourseLiteDTO
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Semester { get; set; }
+    }
+}
